f61f528aceae79db6a52c82e134e1bea4addb016
Gazel averrous <Gazel@ngoprekweb.com>
2020-02-09 15:06:21 +0700

    Initial commit

---
+++ app/src/main/java/com/gazel/apppreference/MainActivity.java
@@ -0,0 +1,43 @@
+package com.gazel.apppreference;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements FragmentForm.onSaveListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MySharedPreference mySharedPreference = new MySharedPreference(this);
+
+        if (mySharedPreference.getName() == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frameLayout, new FragmentForm())
+                    .commit();
+        } else  {
+            FragmentWelcome fragmentWelcome = FragmentWelcome
+                    .newInstance(mySharedPreference.getName(), mySharedPreference.getEmail());
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frameLayout, fragmentWelcome)
+                    .commit();
+        }
+    }
+    @Override
+    public void onSave() {
+        MySharedPreference mySharedPreference = new MySharedPreference(this);
+
+        FragmentWelcome fragmentWelcome = FragmentWelcome
+                .newInstance(mySharedPreference.getName(), mySharedPreference.getEmail());
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frameLayout, fragmentWelcome)
+                .commit();
+    }
+}
---
+++ app/src/main/java/com/gazel/apppreference/MySharedPreference.java
@@ -0,0 +1,41 @@
+package com.gazel.apppreference;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class MySharedPreference {
+    private static final String PREF_NAME = "pref name";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_EMAIL = "email";
+
+    private Context mContext;
+    private SharedPreferences sharedPreferences;
+
+    public MySharedPreference(Context context) {
+        this.mContext = context;
+
+        sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+    }
+
+    public String getName() {
+        return sharedPreferences.getString(KEY_NAME, null);
+    }
+
+    public String getEmail() {
+        return sharedPreferences.getString(KEY_EMAIL, null);
+    }
+
+    public void saveName(String email) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(KEY_EMAIL, email);
+        editor.apply();
+    }
+
+    public void saveEmail(String email) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(KEY_EMAIL, email);
+        editor.apply();
+    }
+}
